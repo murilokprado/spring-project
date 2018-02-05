5a663857cdc4c79c57b6ce2e285b2c106ad78ee7
Murilo Prado <kruzprado@hotmail.com>
2018-02-05 20:03:14 -0200

    :bug: fix bug initial page

--- src/main/java/com/spring/example/controller/LoginController.java
+++ src/main/java/com/spring/example/controller/LoginController.java
@@ -1,12 +1,26 @@
 package com.spring.example.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.spring.example.model.User;
+import com.spring.example.repository.UserInterface;
 
 @Controller
 public class LoginController {
+	
+	
+	@Autowired
+	private UserInterface userInterface;
 	
 	@RequestMapping(method = RequestMethod.GET, path = "/enter")
 	public ModelAndView enter() {
@@ -14,5 +28,12 @@ public class LoginController {
 		
 		return modelAndView;
 	}
-
-}
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/")
+	public ModelAndView listOfUser() {
+		ModelAndView modelAndView = new ModelAndView("user/user-list");
+		
+		modelAndView.addObject("userList", userInterface.findAll());
+		
+		return modelAndView;
+	}}
